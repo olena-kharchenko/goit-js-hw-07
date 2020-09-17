587e1c4ece30ae1ce427d04c312b70106eaa4e5e
olena-kharchenko <harchenko.es1990@gmail.com>
2020-09-17 18:05:10 +0300

    Update task-5.js

--- js/task-5.js
+++ js/task-5.js
@@ -8,3 +8,5 @@ const outputEl = document.querySelector('#name-output');
 inputEl.addEventListener('input', () => {
   outputEl.textContent = inputEl.value === '' ? 'незнакомец' : inputEl.value;
 });
+
+
